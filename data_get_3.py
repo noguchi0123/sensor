f6d17cb918348aef9ebc09607693462ce86dcb0f
noguchi0123 <noguchi@west.sd.keio.ac.jp>
2016-07-27 22:40:50 +0900

    Update and rename data_get.py to data_get_3.py

---
+++ data_get_3.py
@@ -0,0 +1,16 @@
+###test/data_get.py
+
+import serial
+import sys
+import datetime
+argvs = sys.argv
+
+port = argvs[1]
+ser = serial.Serial(port,38400,timeout=10) # 10s-timeout baudrate=38400
+ser.write(b'<RM,>??\r\n') #send command (CR+LF)
+line = ser.readline()
+line = str(datetime.datetime.now()) + str(",") + line.decode('utf-8')
+line = line.split(',')
+print(line[0])
+print(line[1])
+print(line[4])
