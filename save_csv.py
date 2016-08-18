26f3cb66da119b29bc106a8b3f6784da52e76b28
noguchi <noguchi@west.sd.keio.ac.jp>
2016-08-18 19:52:41 +0900

    convert to py3

--- save_csv.py
+++ save_csv.py
@@ -14,7 +14,7 @@ delay = int (argvs[3])
 #ser = serial.Serial('/dev/ttyUSB0',38400,timeout=10) # 10s-timeout baudrate=38400
 ser = serial.Serial(argvs[2],38400,timeout=5) # 10s-timeout baudrate=38400
 
-#ser.write('<RM,>??\r\n') #send command (CR+LF)
+#ser.write(b'<RM,>??\r\n') #send command (CR+LF)
 
 now = datetime.datetime.now() # now no setting
 count = 0
@@ -26,12 +26,12 @@ while True:
         #split_line = line.split(',') #conma de split
         f = open(argvs[1],"w")
         while True:
-                ser.write('<RM,>??\r\n') #send command (CR+LF)
+                ser.write(b'<RM,>??\r\n') #send command (CR+LF)
                 line = ser.readline()
-                print line
+                print(line)
                 f.write(str(count))
                 f.write(",")
-                f.write(line)
+                f.write(str(line))
                 #print now
                 time.sleep(delay)
                 count += delay
