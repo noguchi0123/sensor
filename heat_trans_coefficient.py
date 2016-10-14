d85ca6eb9b3e1bb95fa6b5278a8290134ece32a0
noguchi <noguchi@west.sd.keio.ac.jp>
2016-10-14 17:20:59 +0900

    add a function

--- heat_trans_coefficient.py
+++ heat_trans_coefficient.py
@@ -9,3 +9,9 @@ def heat_trans_coef(*,a_out,thick,heat_trans,a_in,c=False):
 ##surface area
 def surface_area(*,width=0.150,height=0.075,depth=0.150):
     return 2*(width*height+height*depth+depth*width)
+
+##volumetric flow rate
+def volmetric_flow_rate(*,v_wind,hole_area):
+    return v_wind*hole_area
+
+
