fe4119ae5fccd1932674826a8c7fe8ae0d4a6ba5
noguchi <noguchi@west.sd.keio.ac.jp>
2016-10-14 17:13:21 +0900

    add a module

--- heat_trans_coefficient.py
+++ heat_trans_coefficient.py
@@ -1,6 +1,11 @@
+##caluculate heat transmit coefficent 
 def heat_trans_coef(*,a_out,thick,heat_trans,a_in,c=False):
     if c is False:
         coef = 1/((1/a_out)+(thick/heat_trans)+(1/a_in))
     else:
         coef = 1/((1/a_out)+(thick/heat_trans)+(1/c)+(1/a_in))
     return coef
+
+##surface area
+def surface_area(*,width=0.150,height=0.075,depth=0.150):
+    return 2*(width*height+height*depth+depth*width)
